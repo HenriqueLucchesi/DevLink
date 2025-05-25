3b9fcee3025fb17e7e8df884f13b999b99515a1e
HenriqueLucchesi <hlolucchesi2009@gmail.com>
2025-05-25 20:35:07 -0300

    first commit

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/assets/eu.png")
+    img.setAttribute("src", "./assets/eu.png")
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/assets/eu.png")
+    img.setAttribute("src", "./assets/eu.png")
   }
 }
